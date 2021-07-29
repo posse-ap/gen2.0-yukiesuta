38b91f8a036232582c7afe7130d84244e9279d68
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-07-29 19:11:16 +0900

    2021/7/29

---
+++ quizy.js
@@ -0,0 +1,28 @@
+document.getElementById("choiceI1").onclick = function () {
+    document.getElementById("choiceI1").style.backgroundColor = "rgb(41,125,254)"; //選択肢の文字
+    document.getElementById("choiceI1").style.color = "white";  //選択肢の背景
+    AnswerBlockI1.style.display = "block";  //答えの表示
+    choiceI1.classList.add("cannotClick");
+    choiceI2.classList.add("cannotClick");
+    choiceI3.classList.add("cannotClick");
+}
+document.getElementById("choiceI2").onclick = function () {
+    document.getElementById("choiceI1").style.backgroundColor = "rgb(41,125,254)";
+    document.getElementById("choiceI1").style.color = "white";
+    document.getElementById("choiceI2").style.backgroundColor = "rgb(255,80,40)";
+    document.getElementById("choiceI2").style.color = "white";
+    AnswerBlockI23.style.display = "block";
+    choiceI1.classList.add("cannotClick");
+    choiceI2.classList.add("cannotClick");
+    choiceI3.classList.add("cannotClick");
+}
+document.getElementById("choiceI3").onclick = function () {
+    document.getElementById("choiceI1").style.backgroundColor = "rgb(41,125,254)";
+    document.getElementById("choiceI1").style.color = "white";
+    document.getElementById("choiceI3").style.backgroundColor = "rgb(255,80,40)";
+    document.getElementById("choiceI3").style.color = "white";
+    AnswerBlockI23.style.display = "block";
+    choiceI1.classList.add("cannotClick");
+    choiceI2.classList.add("cannotClick");
+    choiceI3.classList.add("cannotClick");
+}
