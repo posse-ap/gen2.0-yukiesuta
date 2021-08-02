6cfa052b832134a758da3805e26c6cddf2230570
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-08-02 17:02:14 +0900

    ループは未完了

--- quizy.js
+++ quizy.js
@@ -2,9 +2,9 @@ document.getElementById("choiceI1").onclick = function () {
     document.getElementById("choiceI1").style.backgroundColor = "rgb(41,125,254)"; //選択肢の文字
     document.getElementById("choiceI1").style.color = "white";  //選択肢の背景
     AnswerBlockI1.style.display = "block";  //答えの表示
-    choiceI1.classList.add("cannotClick");
-    choiceI2.classList.add("cannotClick");
-    choiceI3.classList.add("cannotClick");
+    choiceI1.classList.add("cannotClick");//クリックできなくする
+    choiceI2.classList.add("cannotClick");//クリックできなくする
+    choiceI3.classList.add("cannotClick");//クリックできなくする
 }
 document.getElementById("choiceI2").onclick = function () {
     document.getElementById("choiceI1").style.backgroundColor = "rgb(41,125,254)";
