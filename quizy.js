6a3be3664ca6d511dfbf11c07cf22b1efb1166bd
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-08-22 20:15:51 +0900

    0822

--- quizy.js
+++ quizy.js
@@ -1,3 +1,23 @@
+for(let count =0;count<10;count++){
+let classes = 
+
+'<div class="contain">'+
+    '<h3>'+'ガチで東京の人しか解けない！ #東京の難読地名クイズ'+'</h3>'+
+    '<span class="question">'+' 1.この地名はなんて読む？ '+'</span>'+
+    '<p id="choiceI2" class="box">'+'こうわ'+'</p>'+
+    '<p id="choiceI3" class="box">'+'たかわ'+'</p>'+
+    '<div class="answerITrue" id="AnswerBlockI1">'+
+        '+<h1 class="answer_boxI1">'+'正解！'+'</h1>'+
+        '正解は「たかなわ」です！'+
+    '</div>'+
+    '<div class="answerIFalse" id="AnswerBlockI23">'+
+        '<h1 class="answer_boxI23">'+'不正解！'+'</h1>'+
+        '正解は「たかなわ」です！'+
+    '</div>'+
+'</div>'+
+     {/* <script type="text/javascript" src="quizy.js"></script> */}
+    'document.write(classes);'
+}
 document.getElementById("choiceI1").onclick = function () {
     document.getElementById("choiceI1").style.backgroundColor = "rgb(41,125,254)"; //選択肢の文字
     document.getElementById("choiceI1").style.color = "white";  //選択肢の背景
