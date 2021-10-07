fc580cffd1d2b7d454e52ac61f4a91162ae0e588
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-10-08 00:22:00 +0900

    HTMLの記述ができた

--- writing/writing.js
+++ writing/writing.js
@@ -4,43 +4,48 @@ let option =[
     ["こうじまち","おかとまち","かゆまち"]
 ]
 
-for(let count =0;count<3;count++){
-    //シャッフル前の配列
-    const number = [0,1,2];
-    //シャッフル後の配列（選択肢をシャッフルさせる）
-    const newNumber = [];
-
-    console.log(number)
-
-    //シャッフルの記述
-        while (number.length > 0) {
-            n = number.length;
-            k = Math.floor(Math.random() * n);
-            newNumber.push(number[k]);
-            number.splice(k, 1);
-        }
-        console.log(newNumber[0])
-
-        //本文
-        let classes = 
-        '<div class="contain">'+
-            `<p><span class="question">${count+1}.この地名はなんて読む？</span></p>`+
-            `<img src="${count+1}.png"/>`+
-            `<p id="choice${option [count] [newNumber[0]]}_${count+1}" class="box">${option [count] [newNumber[0]]}</p>`+
-            `<p id="choice${option [count] [newNumber[1]]}_${count+1}" class="box">${option [count] [newNumber[1]]}</p>`+
-            `<p id="choice${option [count] [newNumber[2]]}_${count+1}" class="box">${option [count] [newNumber[2]]}</p>`+
-    //デフォルトではCSSによる非表示
-            `<div class="ans" id="ansT_${count+1}">`+
-                '<div class="answerT">'+'<span class="correct_answer">'+'正解！'+'</span>'+'</div>'+
-                `正解は「${option [count][0]}」です！`+
-            '</div>'+
-            `<div class="ans" id="ansF_${count+1}">`+
-                '<div class="answerT">'+'<span class="incorrect_answer">'+'不正解！'+'</span>'+'</div>'+
-                `正解は「${option [count][0]}」です！`+
-            '</div>'+
-        '</div>';
+for(let count =0;count<option.length;count++){
+    
+    //本文
+    let classes = 
+    '<div class="contain">'+
+    `<p><span class="question">${count+1}.この地名はなんて読む？</span></p>`+
+    `<img src="${count+1}.png"/>`+
+    
+    //デフォルトではCSSによる非表示にしてクリックして適切に表示
+    `<div class="ans" id="ansT_${count+1}">`+
+    '<div class="answerT">'+'<span class="correct_answer">'+'正解！'+'</span>'+'</div>'+
+    `正解は「${option [count][0]}」です！`+
+    '</div>'+
+    `<div class="ans" id="ansF_${count+1}">`+
+    '<div class="answerT">'+'<span class="incorrect_answer">'+'不正解！'+'</span>'+'</div>'+
+    `正解は「${option [count][0]}」です！`+
+    '</div>'+
+    '</div>';
     
     document.currentScript.insertAdjacentHTML('beforebegin',classes);
     
-}
+    for(let j =0;j<3;j++){
 
+        //シャッフル前の配列
+        const number = [0,1,2];
+        //シャッフル後の配列（選択肢をシャッフルさせる）
+        const newNumber = [];
+        console.log(number)
+    
+        //シャッフルの記述
+            while (number.length > 0) {
+                n = number.length;
+                k = Math.floor(Math.random() * n);
+                newNumber.push(number[k]);
+                number.splice(k, 1);
+            }
+            console.log(newNumber[0])
+
+        i = newNumber[j];
+        
+        let optionLoop =
+        `<p id="choice${option [count] [newNumber[i]]}_${count+1}" class="box">${option [count] [newNumber[i]]}</p>`
+        document.currentScript.insertAdjacentHTML('beforebegin',optionLoop);
+    }
+}
