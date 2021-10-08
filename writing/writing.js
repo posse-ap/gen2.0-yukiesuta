3daf6e5d15e4a22c579213e2dae999b54bbd47ae
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-10-09 01:13:38 +0900

    もうあとCSSだけ

--- writing/writing.js
+++ writing/writing.js
@@ -3,22 +3,17 @@ let option =[
     ["かめいど","かめど","かめと"],
     ["こうじまち","おかとまち","かゆまち"]
 ]
-
-
+//問題の数ループ
 for(let count =0;count<option.length;count++){
-
-    //シャッフル前の配列
+    //シャッフル前の配列 ここをうまく問題数に合わせて作ってくれるものがないかな？
     const number = [0,1,2];
-    //シャッフル後の配列（選択肢をシャッフルさせる）let にする
+    //シャッフル後の配列（選択肢をシャッフルさせる）毎回違う配列を準備するためletにする
     let newNumber = [];
-    console.log(number);
-    
     //本文
     let classes = 
     '<div class="contain">'+
     `<p><span class="question">${count+1}.この地名はなんて読む？</span></p>`+
     `<img src="${count+1}.png"/>`+`<div id="imgNext${count}"></div>`+
-    
     //デフォルトではCSSによる非表示にしてクリックして適切に表示
     `<div class="ansT" id="ansT_${count+1}">`+
         '正解！'+
@@ -35,8 +30,8 @@ for(let count =0;count<option.length;count++){
     document.currentScript.insertAdjacentHTML('beforebegin',classes);
 
     
-    
-    for(let j =0;j<3;j++){
+    //選択肢の数ループ
+    for(let j =0;j<option[count].length;j++){
 
         //シャッフルの記述
         while (number.length > 0) {
@@ -45,82 +40,34 @@ for(let count =0;count<option.length;count++){
             newNumber.push(number[k]);
             number.splice(k, 1);
         }
-        console.log(newNumber[0]);
         
         let i = newNumber[j];
         
-
         let optionLoop =
         `<p id="choice${option [count] [newNumber[i]]}_${count+1}" class="box${count}">${option [count] [newNumber[i]]}</p>`;
         document.getElementById(`imgNext${count}`).insertAdjacentHTML('afterEnd', optionLoop);
-        console.log(`imgNext${count}`);
-        console.log(document.getElementById(`imgNext${count}`));
-        console.log(document.getElementById(`choice${option [count] [newNumber[i]]}_${count+1}`));
-        // console.log(document.getElementById(`choice${option [count] [newNumber[i]]}_${count+1}`).indexOf(`${option[0]}`);
-        // document.getElementById(`choice${option [count] [newNumber[k]]}_${count+1}`).onclick = function(){
-        //     document.getElementById(`ansT_${count+1}`).style.display = "block";//答えの表示
-        // }
-        // if(document.getElementById(`choice${option [count] [newNumber[i]]}_${count+1}`).indexOf(`${option[0]}`)){
-        //     document.getElementById(`ansT_${count+1}`).style.display = "block"
-        // }else{
-        //     document.getElementById(`ansF_${count+1}`).style.display = "block"
-        // };
     };
-    // if(document.getElementById(`choice${option [count] [newNumber[k]]}_${count+1}`).onclick.indexOf(document.getElementById(`choice${option [count] [newNumber[i]]}_${count+1}`)) !=-1){
-    //     document.getElementById(`ansT_${count+1}`).style.display = "block"
-    // }else{
-    //     document.getElementById(`ansF_${count+1}`).style.display = "block"
-    // };
-    console.log(document.getElementById(`choice${option[count][0]}_${count+1}`));
-    
-    console.log(document.getElementsByClassName(`box${count}`));
-
 
     function afterPush (){
-        for(let l=0;l<3;l++){
+        for(let l=0;l<option[count].length;l++){
             document.getElementById(`choice${option [count] [`${l}`]}_${count+1}`).style.cursor = "pointer";
             document.getElementById(`choice${option [count] [`${l}`]}_${count+1}`).style.pointerEvents = "none";
         };
     };
-    
-
 
     
     document.getElementById(`choice${option [count] [0]}_${count+1}`).onclick = function(){
         document.getElementById(`ansT_${count+1}`).style.display = "block";
         document.getElementById(`ans_${count+1}`).style.display = "block";
-        afterPush;
-        console.log("aaa");
+        afterPush();
         };
 
     for(let k=0;k<2;k++){
+        //上の２をoption[count].length-1で表したいけどうまくいかない
         document.getElementById(`choice${option [count] [`${k+1}`]}_${count+1}`).onclick = function(){
             document.getElementById(`ansF_${count+1}`).style.display = "block";
             document.getElementById(`ans_${count+1}`).style.display = "block";
-            afterPush;
-
-            for(let l=0;l<3;l++){
-                document.getElementById(`choice${option [count] [`${l}`]}_${count+1}`).style.cursor = "pointer";
-                document.getElementById(`choice${option [count] [`${l}`]}_${count+1}`).style.pointerEvents = "none";
-            };
-
-
-            console.log("aaa");
+            afterPush();
         };
     };
-
-
-    // document.getElementById(`ansT_${count+1}`).style.display = "block";
-
-    // if(document.getElementById(`choice${option [count] [0]}_${count+1}`).onclick){
-    //     document.getElementById(`ansT_${count+1}`).style.display = "block";
-    //     console.log("aaa");
-    // }else if(document.getElementById(`choice${option [count] [1]}_${count+1}`).onclick){
-    //     // document.getElementById(`ansF_${count+1}`).style.display = "block";
-    //     console.log("a");
-    // }else{
-    //     // document.getElementById(`ansF_${count+1}`).style.display = "block";
-    //     console.log("asd");
-    // };
-    
 };
