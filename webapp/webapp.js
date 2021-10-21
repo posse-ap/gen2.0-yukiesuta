1c1eb1036d4e12b89161017707a23b38d71f835f
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-10-21 10:48:10 +0900

    レスポンシブ対応した。ちゃんと動く。

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -1,6 +1,7 @@
 'use strict';
 
   const modal_open = document.getElementById('open');
+  const modal_open_resp = document.getElementById('open_resp');
   const modal_close = document.getElementById('close');
   const modal = document.getElementById('modal');
   const mask = document.getElementById('mask');
@@ -9,6 +10,25 @@
 
     modal.style.display ="block";
     mask.style.display = "block";
+  });
+
+  modal_close.addEventListener('click', function () {
+    modal.style.display ="none";
+    mask.style.display = "none";
+  });
+
+  mask.addEventListener('click', function () {
+    modal.style.display ="none";
+    mask.style.display = "none";
+  });
+
+
+
+  modal_open_resp.addEventListener('click', function () {
+
+    modal.style.display ="block";
+    mask.style.display = "block";
+    body.style.display ="none"
 
     // document.getElementsByClassName('modal').style.display = "block";
     // console.log(document.getElementsByClassName('modal').style.display = "block");
@@ -24,4 +44,5 @@
     modal.style.display ="none";
     mask.style.display = "none";
   });
+
 
