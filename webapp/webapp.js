6ad8f256e90a8718d7e755f3a94dcb74dc77c5b4
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-11-02 14:58:48 +0900

    とりあえず

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -1,56 +1,99 @@
-'use strict';
+"use strict";
 
-  const modal_open = document.getElementById('open');
-  const modal_open_resp = document.getElementById('open_resp');
-  const modal_close = document.getElementById('close');
-  const modal = document.getElementById('modal');
-  const mask = document.getElementById('mask');
-  const post = document.getElementById('posting');
-  const load = document.getElementById('loading');
-  const m_top = document.getElementById('m_top');
+// import flatpickr from 'flatpickr/dist/flatpickr.min.js';
+// import { Japanese } from "flatpickr/dist/l10n/ja.js" //日本語用モジュール
 
-  modal_open.addEventListener('click', function () {
+// flatpickr('#js-datepicker', {
+//     locale : Japanese, // 日本語用モジュールを適用
+//     dateFormat : 'Y.m.d（D）', // 2021.05.24（月）の形式で表示
+//     defaultDate : new Date() // 入力エリアの初期値
+//   });
 
-    modal.style.display ="block";
-    mask.style.display = "block";
-  });
+const modal_open = document.getElementById("open");
+const modal_open_resp = document.getElementById("open_resp");
+const modal_close = document.getElementById("close");
+const modal = document.getElementById("modal");
+const mask = document.getElementById("mask");
+const post = document.getElementById("posting");
+const load = document.getElementById("loading");
+const m_top = document.getElementById("m_top");
 
-  modal_close.addEventListener('click', function () {
-    modal.style.display ="none";
-    mask.style.display = "none";
-  });
+modal_open.addEventListener("click", function () {
+  modal.style.display = "block";
+  mask.style.display = "block";
+});
 
-  mask.addEventListener('click', function () {
-    modal.style.display ="none";
-    mask.style.display = "none";
-  });
+modal_close.addEventListener("click", function () {
+  modal.style.display = "none";
+  mask.style.display = "none";
+});
+
+mask.addEventListener("click", function () {
+  modal.style.display = "none";
+  mask.style.display = "none";
+});
+
+modal_open_resp.addEventListener("click", function () {
+  modal.style.display = "block";
+  mask.style.display = "block";
+  body.style.display = "none";
+
+  // document.getElementsByClassName('modal').style.display = "block";
+  // console.log(document.getElementsByClassName('modal').style.display = "block");
+  // mask.classList.remove('hidden');
+});
+
+modal_close.addEventListener("click", function () {
+  modal.style.display = "none";
+  mask.style.display = "none";
+});
+
+mask.addEventListener("click", function () {
+  modal.style.display = "none";
+  mask.style.display = "none";
+});
+
+post.addEventListener("click", function () {
+  load.style.display = "block";
+  m_top.style.display = "none";
+  post.style.display = "none";
+});
 
 
+var ctx2 = document.getElementById("yourchart");
 
-  modal_open_resp.addEventListener('click', function () {
+var yourChart = new Chart(ctx2, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Green", "Blue"],
+    datasets: [
+      {
+        data: [10, 20, 30],
+        backgroundColor: ["#f88", "#484", "#48f"],
+        weight: 100,
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+  },
+});
 
-    modal.style.display ="block";
-    mask.style.display = "block";
-    body.style.display ="none"
+var ctx3 = document.getElementById("hischart");
 
-    // document.getElementsByClassName('modal').style.display = "block";
-    // console.log(document.getElementsByClassName('modal').style.display = "block");
-    // mask.classList.remove('hidden');
-  });
-
-  modal_close.addEventListener('click', function () {
-    modal.style.display ="none";
-    mask.style.display = "none";
-  });
-
-  mask.addEventListener('click', function () {
-    modal.style.display ="none";
-    mask.style.display = "none";
-  });
-
-
-  post.addEventListener('click',function(){
-    load.style.display = "block";
-    m_top.style.display = "none";
-    post.style.display = "none";
-  });
+var yourChart = new Chart(ctx3, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Green", "Blue"],
+    datasets: [
+      {
+        data: [10, 20, 30],
+        backgroundColor: ["#f88", "#484", "#48f"],
+        weight: 100,
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+  },
+});
