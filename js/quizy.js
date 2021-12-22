4996cdc1bef1bab3a66646a5a947ed2ee38c8bf0
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-12-22 11:21:57 +0900

    いマサラタウンだけど画像を保存した

--- js/quizy.js
+++ js/quizy.js
@@ -1,16 +1,16 @@
 //配列
 const pic = [
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/6a235aaa10f0bd3ca57871f76907797b.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/0b6789cf496fb75191edf1e3a6e05039.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/23e698eec548ff20a4f7969ca8823c53.png",
-    "https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png",
-    "https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png",
-    "https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png",
-];//写真
+    "../img/tokyo1.png",
+    "../img/tokyo2.png",
+    "../img/tokyo3.png",
+    "../img/tokyo4.png",
+    "../img/tokyo5.png",
+    "../img/tokyo6.png",
+    "../img/tokyo7.png",
+    "../img/tokyo8.png",
+    "../img/tokyo9.png",
+    "../img/tokyo10.png",
+    ];//写真
 
 box1 = [
     ["たかなわ","たかわ","こうわ"],
