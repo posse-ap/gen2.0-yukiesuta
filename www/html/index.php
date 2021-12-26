6060549b340b14f20e2e9e287f1e67711c6f9909
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-12-26 22:07:52 +0900

    mysql導入

---
+++ www/html/index.php
@@ -0,0 +1,20 @@
+<!-- 名前index.phpにした方がいい？？？？ -->
+
+<!DOCTYPE html>
+<html lang="jp">
+  <head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>kuizy</title>
+  <link rel="stylesheet" href="/css/quizy.css" />
+  </head>
+  <body>
+  <?php for ($i=0; $i < 3; $i++) { ?>
+    <div class="contain">
+      <h3>ガチで東京の人しか解けない！<?= "hoje"; ?> #東京の難読地名クイズ</h3>
+    </div>
+    <?php } ?>
+    <script type="text/javascript" src="/js/quizy.js"></script>
+  </body>
+</html>
