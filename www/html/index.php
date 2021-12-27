b4a32a436dc7f0a8a00b7d4d970e6bb3b6af1216
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-12-27 23:23:28 +0900

    データベースにアクセスできない

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,34 @@
+<?php // データベースに接続するために必要なデータソースを変数に格納
+  // mysql:host=ホスト名;dbname=データベース名;charset=文字エンコード
+$dsn = 'mysql:host=db;dbname=quizy;charset=utf8';
+ 
+  // データベースのユーザー名
+$user = 'yukiho';
+ 
+  // データベースのパスワード
+$password = 'password';
+
+echo phpinfo();
+ 
+// tryにPDOの処理を記述
+try {
+ 
+  // PDOインスタンスを生成
+  $dbh = new PDO($dsn, $user, $password);
+ 
+// エラー（例外）が発生した時の処理を記述
+} catch (PDOException $e) {
+ 
+  // エラーメッセージを表示させる
+  echo 'データベースにアクセスできません！' . $e->getMessage();
+ 
+  // 強制終了
+  exit;
+ 
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="jp">
   <head>
