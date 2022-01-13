7b8acd163898923e70c054bdeb00f449a9d75c50
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-01-13 21:34:21 +0900

    コミット

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -6,12 +6,12 @@ big_question_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 question_title VARCHAR(225) NOT NULL
 );
 INSERT INTO questions (question_title) VALUES
-('東京の難読地名クイズ'),
-('広島の難読地名クイズ');
+('東京'),
+('広島');
 
--- 別のテーブルも作れる
+-- 別のテーブルも作れる テーブルは一つにまとめる、全体スケジュール参照
 CREATE TABLE choices (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+choices_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 choice1 VARCHAR(225) NOT NULL,
 choice2 VARCHAR(225) NOT NULL,
 choice3 VARCHAR(225) NOT NULL
@@ -19,3 +19,15 @@ choice3 VARCHAR(225) NOT NULL
 INSERT INTO choices (choice1,choice2,choice3) VALUES
 ('たかなわ','たかわ','こうわ'),
 ('かめいど','かめど','かめと');
+
+
+-- -- 広島
+-- CREATE TABLE hiroshima_choices (
+-- hiroshima_choice_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- choice1 VARCHAR(225) NOT NULL,
+-- choice2 VARCHAR(225) NOT NULL,
+-- choice3 VARCHAR(225) NOT NULL
+-- );
+-- INSERT INTO choices (hiroshima_choice1,hiroshima_choice2,hiroshima_choice3) VALUES
+-- ('いばらいち','せいげんち','いのはらし'),
+-- ('かるが','いがるけ','かりどめや');
--- www/html/index.php
+++ www/html/index.php
@@ -19,56 +19,47 @@ try {
   exit;
 }
 
+// 東京or広島
+  // prepare:ただ文章（変数込みのsql文を用意する ：の後に後で代入する
+  $Title = $dbh->prepare('SELECT * FROM questions WHERE big_question_id = :big_question_id');
+  // execute()が代入を実行（データベースから分取ってくる）する、引数に配列を入れる
+  $Title->execute(array(':big_question_id'=>$_REQUEST['big_question_id']));
+  // fetchは1行目だけを取ってくる（配列１こ）、fetchAllは全部取ってくる（二次元配列）
+  $Titles = $Title->fetch();
 
-// 12/31 
-// データベースに接続してurl毎に表示内容を変える操作
-// print_rで確認してみたけど、配列は取得できてた。select文はOK？？
-// 65行目あたりのDatasの取得ができていない？？
-// でもprint_rで表示される配列おかしくない？？？
-
-$Data = $dbh->prepare('SELECT * FROM questions WHERE big_question_id = :big_question_id');
-$Data->execute(array('big_question_id'=>$_REQUEST['big_question_id']));
-// fetchは1行目だけを取ってくる（配列１こ）、fetchAllは全部取ってくる（二次元配列）
-$Datas = $Data->fetch();
-
-
-echo($_REQUEST['big_question_id']);
-
-print_r($Datas);
-
+// 東京の選択肢
+$tokyo_choice = $dbh->prepare('SELECT * FROM tokyo_choices WHERE tokyo_choices_id = :tokyo_choices_id');
+$tokyo_choice->execute(array('tokyo_choices_id'=>$_REQUEST['tokyo_choices_id']));
+$tokyo_choices = $tokyo_choice->fetch();
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>kuizy</title>
-  <link rel="stylesheet" href="/css/quizy.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>kuizy</title>
+    <link rel="stylesheet" href="/css/quizy.css" />
   </head>
   <body>
     <?php for ($i=0; $i < 3; $i++) { ?>
       <div class="contain">
-        <!-- <?php print($hyouji['']);?> -->
+        繰り返し表示される文章
       </div>
       <?php } ?>
+
+      <?php print_r($tokyo_choices) ?>
+      
+      <?php  print_r($Titles) ?>
       
       <h3>ガチで
-        <?= $Datas[1] ?>
-        <!-- <?php echo htmlspecialchars($_GET["id"]) ;?> -->
+        <?= $Titles[1] ?>
+        <?= $tokyo_choices[1] ?>
         の人しか解けない！ #
         <?php echo htmlspecialchars($_GET["big_question_id"]);?>
-        <!-- <?php print($Datas["big_question_id"]);?> -->
         </h3>
-
-    
-
-
-
-
     <script type="text/javascript" src="/js/quizy.js"></script>
   </body>
 </html>
